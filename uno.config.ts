cde6f1af6842a6f02b9df8378fdd180ef0d6d839
pkc918 <xiaopan2000918@gmail.com>
2023-12-12 22:04:11 +0800

    initial project

---
+++ uno.config.ts
@@ -0,0 +1,10 @@
+// uno.config.ts
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+    // ...UnoCSS options
+    shortcuts: {
+        "bg-main": "bg-[#cff0da]",
+        "title-main": "text-[#3ac569]"
+    }
+})
