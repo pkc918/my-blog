5be9603b3750e38fa6f5d3fd8ac6cb1e9cedbaeb
pkc918 <xiaopan2000918@gmail.com>
2023-12-17 14:33:31 +0800

    feat: icons use component style

---
+++ src/views/Projects.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import Github from "@/icons/Github.vue";
+</script>
+
+<template>
+<div class="w-full flex flex-col items-center">
+  <div class="flex">
+    <div class="text-7"><Github/></div>
+    <h2>我的项目</h2>
+  </div>
+  <div class="max-w-300 w-full h-10 bg-amber pt-10">
+
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
