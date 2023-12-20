4e97e20c22abfe85a8ae2da37c0d4987555e8e6b
pkc918 <xiaopan2000918@gmail.com>
2023-12-20 22:25:26 +0800

    feat: projects item style

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,10 +7,17 @@ import Github from "@/icons/Github.vue";
 <div class="w-full flex flex-col items-center">
   <div class="flex">
     <div class="text-7"><Github/></div>
-    <h2>我的项目</h2>
+    <h2>参与的开源项目</h2>
   </div>
-  <div class="max-w-300 w-full h-10 bg-amber pt-10">
-
+  <div class="max-w-300 w-full pt-10">
+    <ul class="w-full flex flex-wrap">
+      <li class="w-[33.33%] box-border px-2" v-for="item in 4" :key="item"
+          slide-enter :style="{ '--stagger': item + 1 }"
+      >
+        <h2>IKun UI</h2>
+        <p>🐔A Svelte.js based UnoCSS UI library that allows you to make websites</p>
+      </li>
+    </ul>
   </div>
 </div>
 </template>
