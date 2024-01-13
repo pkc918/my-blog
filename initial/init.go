566d08ac0d0c1024ec1aa5edd32a5cba49ec560c
pkc918 <xiaopan2000918@gmail.com>
2024-01-13 16:24:28 +0800

    chore: initial flow

--- initial/init.go
+++ initial/init.go
@@ -3,12 +3,34 @@ package initial
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"my-blog/db"
 	"my-blog/router"
+	"os"
 )
 
-func Server() {
+func init() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func ginServer() {
+	port := viper.GetString("server.port")
 	r := gin.Default()
-	fmt.Println("initial")
 	router.AddRoutes(r)
-	r.Run()
+	err := r.Run(fmt.Sprintf(":%s", port))
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func Server() {
+	db.Server()
+	ginServer()
 }
